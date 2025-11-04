234cc81c600362ce6eca5d87cf8384b85868bf3a
Alper Çıtak <citak.alper@gmail.com>
2025-11-04 23:32:41 +0100

    use rsbuild for client

---
+++ client/rsbuild.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import { defineConfig } from '@rsbuild/core';
+import { pluginLess } from '@rsbuild/plugin-less';
+import { pluginVue } from '@rsbuild/plugin-vue';
+
+export default defineConfig({
+  plugins: [pluginLess(), pluginVue()],
+  html: {
+    template: './index.html',
+  },
+  source: {
+    entry: {
+      index: './src/main.ts',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
